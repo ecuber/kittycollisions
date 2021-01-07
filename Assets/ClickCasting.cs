8b19e52c98e227a8dbfd816a473f9fa3c9950864
ecuber <37519236+ecuber@users.noreply.github.com>
2021-01-06 21:31:32 -0500

    name change; add click detection+colors

---
+++ Assets/ClickCasting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickCasting : MonoBehaviour
+{
+
+    public TMP_Text selection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo) )
+            {
+                selection.text = "cp: " + hitInfo.collider.name;
+            }
+        }
+    }
+
+}
